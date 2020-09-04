cc06abfeac8dd3e31f9a078c1e27af9d6bb0a708
Damienngo <64823488+Damienngo@users.noreply.github.com>
2020-09-04 16:57:30 +0800

    Delete Unitest.py

--- scrapyproject/Unitest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import scrapyproject
-
-class TestProject(unittest.TestCase):
-    def test_add(self):
-        self.assertTrue(True)
-
-if __name__ == '__main__':
-    unittest.main()
